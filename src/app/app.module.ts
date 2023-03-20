659df2250a8267058dfc5b2683c9ea6ed7b7f189
Celorio2001 <jahir.celorio2001@gmail.com>
2023-03-20 15:22:45 -0500

    Añadiendo mas funcionalidades como mostrar los tracklist y mostrar el artista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
